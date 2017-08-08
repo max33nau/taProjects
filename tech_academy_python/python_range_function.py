bcd07cae55db468b41fffe3b2e183a7bf982f0aa
Max Jacobsen <mtjacobsen33@gmail.com>
2017-08-08 07:37:46 -0700

    range function completed for python course

---
+++ tech_academy_python/python_range_function.py
@@ -0,0 +1,28 @@
+#
+#
+# Python 3.6.1
+#
+# Author: Max Jacobsen
+#
+#
+#
+# Purpose: Complete range() function drills
+#
+#
+#
+
+
+# 1.
+print('task 1:')
+for i in range(4):
+    print(i)
+    
+print('\ntask 2:')
+# 2.
+for i in range(3, -1, -1):
+    print(i)
+
+# 3.
+print('\ntask 3:')
+for i in range(8, 1, -2) :
+    print(i)
