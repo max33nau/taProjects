68247831d961a43c5cbc9d9e02dde03d1f6b2118
Max Jacobsen <mtjacobsen33@gmail.com>
2017-09-12 08:38:47 -0700

    Tried to clean up code to make it more straight forward

--- tech_academy_python/file_mod_project/file_mod.py
+++ tech_academy_python/file_mod_project/file_mod.py
@@ -1,20 +1,7 @@
-#
-#
-# Python 2.7.13
-#
-# Author: Max Jacobsen
-#
-#
-# Module used for file creating and manipulation
-# 
-#
-# 
-#
-
 import os
 import shutil
-import time_detail
-
+from datetime import datetime, timedelta
+import pytz
 
 def create_directory(path) :
     try: 
@@ -36,77 +23,44 @@ def empty_directory_files(directory_path) :
     else :
         print('The file path {} does not exist so there is nothing to empty.'.format(directory_path))
 
-def get_all_modified_files(directory_path) :
-    if os.path.exists(directory_path):
-        changed_files = []
-        for file_object in os.listdir(directory_path):
-            file_path = os.path.join(directory_path, file_object)
-            if(os.path.isfile(file_path) and file_newly_modified(file_path)):
-                print('{} file has been modified or created within the last 24 hours.'.format(file_path))
-                changed_files.append(file_object)
-        return changed_files
-    else:
-        print('The file path {} does not exist, so no modified files found.'.format(directory_path))
-        return []
+def copy_modified_files(src, dest) :
+    if os.path.exists(src) and os.path.exists(dest):
+        mod_found = False
+        for file_obj in os.listdir(src):
+            file_path = os.path.join(src, file_obj)
+            if(os.path.isfile(file_path) and os.path.splitext(file_path)[1] == '.txt'):
+                if(modified_in_last_24_hours(file_path)):
+                    shutil.copy(os.path.join(src, file_obj), os.path.join(dest, file_obj))
+                    mod_found = True
+                    print('Copied file {} from {} to {}.'.format(file_obj,src, dest))
+        if(not mod_found):
+            print('\n No files created or modified within the last 24 hours.')
 
-def file_newly_modified(file_path):
-    most_recent_mod = created_or_mod_time(file_path)
-    return time_detail.less_than_24_hour(most_recent_mod)
-    
 
-def created_or_mod_time(path):
+def modified_in_last_24_hours(file_path):
+    day_ago = datetime.now(pytz.utc) - timedelta(days=1)
+    path_mod_utc = get_file_mod_time(file_path)
+    within_24_hours = (day_ago < path_mod_utc)
+    print('\nChecking file {}: '.format(file_path))
+    print('Last modified or created = {}, \nWithin 24 hours? {}'.format(path_mod_utc, within_24_hours)) 
+    return within_24_hours
+
+def get_file_mod_time(path):
+    path_stat = os.stat(path)
+    # Different os systems handle file creation and modification differently
+    # so this is a more cross platform safe solution to get the time
     path_stat = os.stat(path)
     try: 
-        created = time_detail.utc_from_timestamp(path_stat.st_ctime)
+        created = path_stat.st_ctime
     except:
-        print('created not found')
         created = False
     try:
-        modified = time_detail.utc_from_timestamp(path_stat.st_mtime)
+        modified = path_stat.st_mtime
     except:
-        print('modified not found')
         modified = False
     if(created > modified):
-        return created
+        timestamp = created
     else:
-        return modified
-
-def create_txt_file(path) :
-    try:
-        if not os.path.exists(path):
-            file = open(path, 'w')
-            file.close()
-    except:
-        print('Error creating file path.')
-
-def create_all_txt_files(base, paths) :
-    for path in paths :
-        create_txt_file(os.path.join(base, path))
-
-def move_file(start, end) :
-    try:
-        shutil.move(start, end)
-        print('\n Moved file {} to {}.'.format(start,end))
-    except:
-        print('Error occured while trying to move file')
-        
-def move_all_files(base1, base2, files) :
-    for fileName in files:
-        move_file(os.path.join(base1, fileName), os.path.join(base2, fileName))
-    print('\n \n Successfully moved all files from \n {} to {}.'.format(base1, base2))
-
-
-def copy_file(src, dest):
-    try:
-        shutil.copy(src, dest)
-        print('\n Copied file {} to {}.'.format(src, dest))
-    except:
-        print('Error occured while trying to copy file')
-        
-def copy_all_files(base1, base2, files) :
-    for fileName in files:
-        copy_file(os.path.join(base1, fileName), os.path.join(base2, fileName))
-    print('\n \n Successfully copied all files from \n {} to {}.'.format(base1, base2))
-
-
-
+        timestamp = modified
+    path_time = datetime.utcfromtimestamp(timestamp)
+    return pytz.utc.localize(path_time)
--- tech_academy_python/file_mod_project/python_file_mod_checker.py
+++ tech_academy_python/file_mod_project/python_file_mod_checker.py
@@ -19,13 +19,10 @@ import file_mod
 desktop_path = 'C:\\Users\\mtjac\\Desktop\\'
 watched_folder = desktop_path + 'Folder_Watched'
 destination_folder = desktop_path + 'Folder_Modified'
-directory_paths = [ watched_folder, destination_folder ] 
+directory_paths = [ watched_folder, destination_folder ]
+
 
 if __name__ == '__main__' :
     file_mod.create_directories(directory_paths)
     file_mod.empty_directory_files(destination_folder)
-    recent_mod_files = file_mod.get_all_modified_files(watched_folder)
-    if(len(recent_mod_files) is not 0):
-        file_mod.copy_all_files(watched_folder, destination_folder, recent_mod_files)
-    else:
-        print('No modified or newly created files found in watched foler.')
+    file_mod.copy_modified_files(watched_folder, destination_folder)
