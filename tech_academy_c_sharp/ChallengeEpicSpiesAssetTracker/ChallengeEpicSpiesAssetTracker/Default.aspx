c023b52de029bd7d15d6b35354ac713bd700ac8e
Max Jacobsen <mtjacobsen33@gmail.com>
2017-10-20 15:15:13 -0700

    Challenge epic spies asset tracker completed

---
+++ tech_academy_c_sharp/ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <asp:Image ID="Image1" runat="server" ImageUrl="Assets/epic-spies-logo.jpg" Height="150px" />
+    <h2>     
+        <span class="auto-style1">Asset Performance Tracker</span>
+    </h2>
+    <form id="form1" runat="server">
+        <div>
+
+            Asset Name:
+            <asp:TextBox ID="assetNameInput" runat="server"></asp:TextBox>
+&nbsp;<br />
+            <br />
+            Elections Rigged:
+            <asp:TextBox ID="electionsRiggedInput" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="actsOfSubterfugeInput" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addAssetButton" runat="server" OnClick="addAssetButton_Click" Text="Add Asset" />
+
+        </div>
+        <p>
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ tech_academy_c_sharp/ChallengeEpicSpiesAssetTracker/ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ViewState.Add("Names", new string[0]);
+                ViewState.Add("Elections", new int[0]);
+                ViewState.Add("Subterfuges", new int[0]);
+            }
+        }
+
+        protected void addAssetButton_Click(object sender, EventArgs e)
+        {
+            string[] names = (string[])ViewState["Names"];
+            int[] elections = (int[])ViewState["Elections"];
+            int[] subterfuges = (int[])ViewState["Subterfuges"];
+
+            if (string.IsNullOrEmpty(assetNameInput.Text) ||
+                string.IsNullOrEmpty(electionsRiggedInput.Text) ||
+                string.IsNullOrEmpty(actsOfSubterfugeInput.Text) )
+                return;
+
+            string assetName = assetNameInput.Text;
+            int electionsRigged = int.Parse(electionsRiggedInput.Text);
+            int subterfugesPerformed = int.Parse(actsOfSubterfugeInput.Text);
+
+            int newArrayLength = names.Length + 1;
+            
+            Array.Resize(ref names, newArrayLength);
+            int lastIndex = names.GetUpperBound(0);
+            names[lastIndex] = assetName;
+
+            Array.Resize(ref elections, newArrayLength);
+            elections[lastIndex] = electionsRigged;
+
+            Array.Resize(ref subterfuges, newArrayLength);
+            subterfuges[lastIndex] = subterfugesPerformed;
+
+            resultLabel.Text = string.Format("Total Elections Rigged: {0} <br /> Average Acts of Subterfuge per Asset {1:N2} <br /> (Last Asset you Added: {2})", 
+                elections.Sum(),
+                subterfuges.Average(),
+                names[lastIndex]
+                );
+
+            ViewState.Add("Names", names);
+            ViewState.Add("Elections", elections);
+            ViewState.Add("Subterfuges", subterfuges);
+
+            assetNameInput.Text = "";
+            electionsRiggedInput.Text = "";
+            actsOfSubterfugeInput.Text = "";
+        
+        }
+    }
+}
