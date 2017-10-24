4555a97663c0f0db3a189e548eea9afdd1e46c97
Max Jacobsen <mtjacobsen33@gmail.com>
2017-10-23 22:29:55 -0700

    finished mega challenge

---
+++ tech_academy_c_sharp/MegaChallengeCasino/MegaChallengeCasino/Default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MegaChallengeCasino.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            color: #FF0000;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <asp:Image ID="image1" runat="server" />
+        <asp:Image ID="image2" runat="server" />
+        <asp:Image ID="image3" runat="server" />
+    </div>
+    <br />
+    <br />
+    <form id="form1" runat="server">
+        <div>
+
+            Your Bet:
+            <asp:TextBox ID="betAmmountInput" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="pullLeverButton" runat="server" Text="Pull The Lever!" OnClick="pullLeverButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Player&#39;s Money:
+            <asp:Label ID="playersTotalMoney" runat="server"></asp:Label>
+
+        </div>
+    </form>
+    <h3>Game Details</h3>
+    <p style="margin-top: 0px">
+        1 Cherry - x2 Your Bet <br />
+        2 Cherries - x3 Your Bet <br />
+        3 Cherries - x4 Your Bet <br />
+        3 7's - Jackpot - x100 Your Bet <br /> <br />
+        <span class="auto-style1">
+        <strong>HOWEVER</strong>... If there's even one BAR you win nothing.
+    </span>
+    </p>
+</body>
+</html>
---
+++ tech_academy_c_sharp/MegaChallengeCasino/MegaChallengeCasino/Default.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MegaChallengeCasino
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string[] imageUrls = new string[12] 
+            { "Bar.png", "Bell.png", "Cherry.png", "Clover.png", "Diamond.png",
+              "HorseShoe.png", "Lemon.png", "Orange.png", "Plum.png", "Seven.png",
+              "Strawberry.png", "Watermellon.png" };
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                Image[] imageIds = getImageIds();
+                spinImages(imageIds);
+                setPlayersMoney(100);
+            }
+        }
+
+        protected void pullLeverButton_Click(object sender, EventArgs e)
+        {
+            double playersBet = 0.0;
+            if (!validBet(betAmmountInput.Text, out playersBet)) return;
+            Image[] imageIds = getImageIds();
+            spinImages(imageIds);
+            evalSpin(playersBet, imageIds);
+        }
+
+        protected bool validBet(string betAmmount, out double playersBet)
+        {
+            double playersTotalMoney = getPlayersTotalMoney();
+            if (!Double.TryParse(betAmmountInput.Text, out playersBet))
+            { resultLabel.Text = "Invalid Bet"; return false; }
+            else if (playersTotalMoney - playersBet < 0)
+            { resultLabel.Text = "Insufficient Funds"; return false; }
+            else return true;
+        }
+
+        protected Image[] getImageIds()
+        {
+            Image[] imageIds = { image1, image2, image3 };
+            return imageIds;
+        }
+
+        protected void spinImages(Image[] imageIds)
+        {
+            for (int i = 0; i < imageIds.Length; i++)
+            {
+                int randomNumber = getRandomNumber(0, imageUrls.Length - 1);
+                setImage(imageIds[i], imageUrls[randomNumber]);
+            }
+        }
+
+        Random random = new Random();
+        protected int getRandomNumber(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+
+        protected void setImage(Image image, string imageUrl)
+        {
+            string imageFullPath = "Images/" + imageUrl;
+            image.ImageUrl = imageFullPath;
+        }
+
+        protected string stripImageUrl(string imageUrl)
+        {
+            int start = imageUrl.IndexOf("/") + 1;
+            int end = imageUrl.IndexOf(".") - start;
+            return imageUrl.Substring(start, end);
+        }
+
+        protected void evalSpin(double playersBet, Image[] imageIds)
+        {
+            double winnings = 0.0;
+            if (barImageFound(imageIds)) winnings = -playersBet;
+            else if (jackbpotFound(imageIds)) winnings = playersBet * 100;
+            else { winnings = numberOfCherries(imageIds, playersBet); }
+            evalWinnings(winnings, playersBet);
+        }
+
+        protected bool barImageFound(Image[] imageIds)
+        {
+            for (int i = 0; i < imageIds.Length; i++)
+            {
+                string imageType = stripImageUrl(imageIds[i].ImageUrl);
+                if (imageType == "Bar") return true;
+            }
+            return false;
+        }
+
+        protected bool jackbpotFound(Image[] imageIds)
+        {
+            for (int i = 0; i < imageIds.Length; i++)
+            {
+                string imageType = stripImageUrl(imageIds[i].ImageUrl);
+                if (imageType != "Seven") return false;
+            }
+            return true;
+        }
+
+        protected double numberOfCherries(Image[] imageIds, double playersBet)
+        {
+            int cherries = 0;
+            for (int i = 0; i < imageIds.Length; i++)
+            {
+                string imageType = stripImageUrl(imageIds[i].ImageUrl);
+                if (imageType == "Cherry") cherries++;
+            }
+            return priceWinningsByCherries(playersBet, cherries);
+        }
+
+        protected double priceWinningsByCherries(double playersBet, int numberOfCherries)
+        {
+            if (numberOfCherries == 0) return -playersBet;
+            else return playersBet * (numberOfCherries + 1);
+        }
+
+
+        protected void evalWinnings(double winnings, double playersBet)
+        {
+            double playersTotalMoney = getPlayersTotalMoney();
+            setPlayersMoney(playersTotalMoney + winnings);
+            if (winnings < 0) userLostMoney(winnings);
+            else userWonMoney(winnings, playersBet);
+        }
+
+        protected void userLostMoney(double ammountLost)
+        {
+            resultLabel.Text = String.Format("Sorry, you lost {0:C2}. Better luck next time.", Math.Abs(ammountLost));
+        }
+
+        protected void userWonMoney(double ammountWon, double playersBet)
+        {
+            resultLabel.Text = String.Format("You bet {0:C2} and won {1:C2}", playersBet, ammountWon);
+        }
+
+        protected double getPlayersTotalMoney()
+        {
+            return Double.Parse(playersTotalMoney.Text, System.Globalization.NumberStyles.Currency);
+        }
+
+        protected void setPlayersMoney(double ammount)
+        {
+            playersTotalMoney.Text = String.Format("{0:C2}", ammount);
+        }
+
+    }
+}
