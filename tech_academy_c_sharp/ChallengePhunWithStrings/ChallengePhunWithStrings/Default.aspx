d9dff4c97fa7a0269fb8360d4622472d01c46b58
Max Jacobsen <mtjacobsen33@gmail.com>
2017-10-24 23:13:11 -0700

    Completed challenge Phun with strings

---
+++ tech_academy_c_sharp/ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengePhunWithStrings.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            1. Reverse the name &quot;Bob Tabor&quot;<br />
+            <br />
+            Output =
+            <asp:Label ID="reverseNameLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            2. Reverse this sequence: &quot;Luke,Leia, Han,Chewbacca&quot;<br />
+            <br />
+            Output =
+            <asp:Label ID="reverseSequenceLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            3. Use the sequence to create ascii art:<br />
+            <br />
+            Output:<br />
+            <br />
+            <asp:Label ID="asciiLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            4. Solve the puzzle<br />
+            <br />
+            Output:&nbsp;&nbsp;
+            <asp:Label ID="puzzleLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ tech_academy_c_sharp/ChallengePhunWithStrings/ChallengePhunWithStrings/Default.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePhunWithStrings
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // 1. Reverse your name
+            string name = "Bob Tabor";
+            string reversedName = "";
+            for (int i = name.Length - 1; i >= 0; i-- )
+            {
+                reversedName += name[i].ToString();
+            }
+            reverseNameLabel.Text = reversedName;
+
+            // 2. Reverse this sequence
+            string names = "Luke,Leia,Han,Chewbacca";
+            string[] arrayOfNames = names.Split(',');
+            string reversedSequence = "";
+            for (int i = arrayOfNames.Length - 1; i >= 0; i-- )
+            {
+                reversedSequence += arrayOfNames[i];
+                if (i != 0) { reversedSequence += ',';  }
+            }
+            reverseSequenceLabel.Text = reversedSequence;
+
+            // 3. Use the sequence to create ascii art:
+            string asciiArt = "";
+            int totalLength = 14;
+            for (int i = 0; i < arrayOfNames.Length; i++)
+            {
+                int asciis = (totalLength - arrayOfNames[i].Length) / 2 + arrayOfNames[i].Length;
+                asciiArt += arrayOfNames[i].PadLeft(asciis, '*').PadRight(totalLength, '*') + "<br />";
+            }
+            asciiLabel.Text = asciiArt;
+
+            // 4. Solve this puzzle:
+            // output should be: Now is the time for all good men to come to the aid of their country. 
+            string puzzle = "NOW IS ZHEremove-me ZIME FOR ALL GOOD MEN ZO COME ZO ZHE AID OF ZHEIR COUNZRY.";
+            string removeString = "remove-me";
+            int removeMeIndex = puzzle.IndexOf(removeString);
+            puzzleLabel.Text = puzzle.Replace('Z', 'T')
+                .ToLower()
+                .Remove(removeMeIndex, removeString.Length)
+                .Remove(0, 1)
+                .Insert(0, "N");
+        }
+    }
+}
