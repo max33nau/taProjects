84b09a65ca4b58e55512512f8b2249d07e130a56
Max Jacobsen <mtjacobsen33@gmail.com>
2017-11-12 18:52:15 -0800

    Completes challenge for MVC course

---
+++ tech_academy_c_sharp/FirstChallenge/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comicBooks = ComicBookManager.GetComicBooks();
+            return View(comicBooks);
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            ComicBook comicBook = ComicBookManager.GetComicBooks().FirstOrDefault(c => c.ComicBookId == id);
+            if (comicBook == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(comicBook);
+
+        }
+    }
+
+
+}
---
+++ tech_academy_c_sharp/HelloMVC/HelloMVC/Models/HelloModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloMVC.Models
+{
+    public class HelloModel
+    {
+        public static List<Car> GetCars()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car { Id = 1, Make = "BMW", Model ="528i"},
+                new Car { Id = 2, Make = "Hyandai", Model="Elantra"}
+            };
+
+            return cars;
+        }
+
+
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+    }
+}
